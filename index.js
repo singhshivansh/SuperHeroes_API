870c4c345e8a44fdc12acf7b19f79d1f0e2d89f8
singhshivansh <singhshivansh12@gmail.com>
2021-10-09 16:11:57 +0530

    added main index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path      = require('path');
 const app       = express();
 const port      = 3000;
 const fs        = require('fs');
-const mar       = require('./All Marvel Movies.json');
+const mar       = require('./Marvel/All Marvel Movies.json');
 const _         = require('lodash');
 
 let marvel_json;
