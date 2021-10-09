4b27f41f7fc66ef6aaa51853cdc65a01e7ed307a
singhshivansh <singhshivansh12@gmail.com>
2021-10-09 21:18:34 +0530

    added get dc movie API

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path      = require('path');
 const app       = express();
 const port      = process.env.PORT || 3000;
 const fs        = require('fs');
-const mar       = require('./Marvel/All Marvel Movies.json');
+const mar       = require('./Marvel/All Marvel Movies.json');   // MARVEL JSON file
+const dc        = require('./DC/All DC Movies.json');           // DC JSON file
 const _         = require('lodash');
 
 let marvel_json;
@@ -18,16 +19,46 @@ app.get('/', (req, res)=>{
     res.send('Welcome to this wonderful API! By  : Shivansh <3 ');
 })
 
+
+//get MARVEL movie API
 app.get('/get_marvel_movie', (req, res)=>{
     const body = req.query;
     const movie_name    = body.movie_name;
     const actor_name    = body.actor_name;
     let film = mar;
     if (movie_name)
-        film = film.filter(e=>{return e.name.toLowerCase().includes(movie_name.toLowerCase())});
+        film = film.filter(e=>{
+            if(e.name)
+                return e.name.toLowerCase().includes(movie_name.toLowerCase())
+        });
     if (actor_name)
-        film = film.filter(e=>{return e.stars[0].toLowerCase().includes(actor_name.toLowerCase())});
+        film = film.filter(e=>{
+            if(e.stars[0])
+                return e.stars[0].toLowerCase().includes(actor_name.toLowerCase())
+        });
     res.end(JSON.stringify(film));
+})
+
+//get DC movie API
+app.get('/get_dc_movie', (req, res)=>{
+    const body = req.query;
+    const movie_name = body.movie_name;
+    const actor_name = body.actor_name;
+
+    let film = dc;
+    if(movie_name)
+        film = film.filter(e=>{
+            if(e.name)
+                return e.name.toLowerCase().includes(movie_name.toLowerCase())
+        });
+
+    if (actor_name)
+        film = film.filter(e=>{
+            if(e.stars[0])
+                return e.stars[0].toLowerCase().includes(actor_name.toLowerCase())
+        });
+
+    res.send(JSON.stringify(film));
 })
 
 
